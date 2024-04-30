9e2e5f029087420b1f9a6f589692d9fac59e32dc
1105-Chawla-Saurabh <saurabhc@nevada.unr.edu>
2024-04-30 19:41:56 +0000

    start/base

---
+++ images.c
@@ -0,0 +1,18 @@
+//Authors: Saurabh Chawla & Lukas Farias
+//Date: 05/07/2024
+//Objective: "Erinstagram" - Create a program that allows the user to load a new image in which they are able to edit by cropping, dimming, birghtening, or rotating. 
+
+#include <stdio.h>
+
+#define VERT_CAP 10
+#define HORI_CAP 10
+
+void loadImage()
+void displayImage()
+void editImage()
+void crop image()
+void dimImage()
+void brightenImage()
+void rotateImage_90()
+
+int main(){
