b1032e483a0f9ac627682a623c190e131783d561
1105-Farias-Lukas <157869661+1105-Farias-Lukas@users.noreply.github.com>
2024-05-03 22:38:45 -0700

    i think the function is doneish

--- bright_dim.c
+++ bright_dim.c
@@ -6,22 +6,21 @@
 #define MAX_COLS 100
 
 
-brightenImage(char img[][MAX_COLS], int rows, int cols);
+brightenImage(char img[][MAX_COLS], int newImage[][MAX_COLS], int rows, int cols);
 
 int main(){
 
 int image[MAX_ROWS][MAX_COLS];
 int rows = 10, cols = 10;
+int newImage[MAX_ROWS][MAX_COLS];
 
 	for(int i = 0; i < rows; i++){
 	    for(int j = 0; j < cols; j++){
-    		switch (j<5){
-	   		 case 1: 
+    		if (j<5){
             	if (i<2 || i>8){
           			image[i][j] = 2;
             	}else image[i][j]= 4;
-             	break;
-             case 0:
+            }else{
     			if (i<5){
             		image[i][j] = 3
                 }else image[i][[j] = 1;
@@ -35,17 +34,20 @@ int rows = 10, cols = 10;
 	return 0;
 }
 
-void brighten_dimImage(int img[][MAX_COLS], int rows, int cols){
+void brighten_dimImage(int img[][MAX_COLS], int newImage[][MAX_COLS], int rows, int cols){
 	int x = 0;
     printf("brighten or dim by how much? ");
     scanf("%d", x);
     
-    
-    }
-    
     for(int i = 0; i < rows; i++){
 	    for(int j = 0; j < cols; j++){
-}
-
-
-
+            
+			newImage[i][j] = img[i][j] + x;
+            
+        	if (newImage[i][j] < 0){
+                newImage[i][j] = 0;
+			}else if (newImage[i][j] > 4){
+                newImage[i][j] = 4;
+            }
+        }	
+    }
