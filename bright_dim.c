e5e37155fa2880cdaa1d546c1f6a07c0a5ab7910
1105-Farias-Lukas <157869661+1105-Farias-Lukas@users.noreply.github.com>
2024-05-03 23:02:41 -0700

    It compiled!

--- bright_dim.c
+++ bright_dim.c
@@ -6,7 +6,8 @@
 #define MAX_COLS 100
 
 
-brightenImage(char img[][MAX_COLS], int newImage[][MAX_COLS], int rows, int cols);
+void brighten_dimImage(int img[][MAX_COLS], int newImage[][MAX_COLS], int rows, int cols);
+void displayImage(int img[][MAX_COLS], int rows, int cols);
 
 int main(){
 
@@ -14,40 +15,68 @@ int image[MAX_ROWS][MAX_COLS];
 int rows = 10, cols = 10;
 int newImage[MAX_ROWS][MAX_COLS];
 
-	for(int i = 0; i < rows; i++){
-	    for(int j = 0; j < cols; j++){
-    		if (j<5){
-            	if (i<2 || i>8){
-          			image[i][j] = 2;
-            	}else image[i][j]= 4;
+  for(int i = 0; i < rows; i++){
+      for(int j = 0; j < cols; j++){
+        if (j<5){
+              if (i<2 || i>7){
+                image[i][j] = 4;
+              }else image[i][j]= 2;
             }else{
-    			if (i<5){
-            		image[i][j] = 3
-                }else image[i][[j] = 1;
-                break:
+          if (i<5){
+                image[i][j] = 3;
+                }else image[i][j] = 1;
             }
         }
     }
-    brightenImage(image, rows, cols);
-	
-	
-	return 0;
+    displayImage(image, rows, cols);
+    brighten_dimImage(image, newImage, rows, cols);
+    displayImage(newImage, rows, cols);
+
+  return 0;
 }
 
 void brighten_dimImage(int img[][MAX_COLS], int newImage[][MAX_COLS], int rows, int cols){
-	int x = 0;
-    printf("brighten or dim by how much? ");
-    scanf("%d", x);
-    
-    for(int i = 0; i < rows; i++){
-	    for(int j = 0; j < cols; j++){
-            
-			newImage[i][j] = img[i][j] + x;
-            
-        	if (newImage[i][j] < 0){
-                newImage[i][j] = 0;
-			}else if (newImage[i][j] > 4){
-                newImage[i][j] = 4;
+  int x = 0;
+  printf("brighten or dim by how much? ");
+  scanf("%d", &x);
+
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+      
+      newImage[i][j] = img[i][j] + x;
+      if (newImage[i][j] < 0){
+        newImage[i][j] = 0;
+      }else if (newImage[i][j] > 4){
+            newImage[i][j] = 4;
             }
         }	
     }
+}
+
+void displayImage(int img[][MAX_COLS], int rows, int cols){
+  printf("\nDisplaying image!\n");
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      switch (img[i][j]) {
+      case 0:
+        printf(" ");
+        break;
+      case 1:
+        printf(".");
+        break;
+      case 2:
+        printf("o");
+        break;
+      case 3:
+        printf("O");
+        break;
+      case 4:
+        printf("0");
+        break;
+      }
+    }
+    printf("\n");
+  } 
+}
+
